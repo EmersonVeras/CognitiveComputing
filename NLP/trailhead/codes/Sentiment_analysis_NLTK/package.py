8ea790e22701004c54a90bc816d0e48f4b458c82
EmersonVeras <emersonverasifce@gmail.com>
2022-04-19 14:48:46 -0300

    upload code about sentiment analysis using python and NLTK library

---
+++ NLP/trailhead/codes/Sentiment_analysis_NLTK/package.py
@@ -0,0 +1,7 @@
+import nltk
+
+def download_packages():
+    nltk.download('punkt')
+    nltk.download('twitter_samples')
+    nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
